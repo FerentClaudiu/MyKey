2e7ac754f7a3af051ce9ec822e6c23099a16fd52
FerentClaudiu <32581324+FerentClaudiu@users.noreply.github.com>
2018-03-18 13:28:09 +0200

    update Ui
    update UI

---
+++ MyKey.UI/Controllers/LocksmithController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyKey.UI.Controllers
+{
+    public class LocksmithController : Controller
+    {
+        // GET: Locksmith
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyKey.UI/Models/LocksmithModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyKey.UI.Models
+{
+    public class LocksmithModel
+    {
+    }
+}
